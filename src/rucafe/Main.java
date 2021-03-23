027cbb2c5a55d1cb953b1eaf8fb1364c46b7c5aa
Toshan Vysyaraju <toshanv2000@gmail.com>
2021-03-23 18:38:30 -0400

    [ FIX ] displaying images for buttons

--- src/rucafe/Main.java
+++ src/rucafe/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
         primaryStage.setTitle("Ru Cafe Main Menu");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 450, 400));
         primaryStage.show();
     }
 
