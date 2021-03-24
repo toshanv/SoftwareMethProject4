f25b428a5e86a37bb77d6df7c76bd654e730eed8
Christopher Nguyen <christophernguyen41@gmail.com>
2021-03-24 15:21:57 -0400

    [add] donut window

---
+++ src/rucafe/DonutController.java
@@ -0,0 +1,5 @@
+package rucafe;
+
+public class DonutController {
+
+}
--- src/rucafe/MainMenuController.java
+++ src/rucafe/MainMenuController.java
@@ -1,4 +1,27 @@
 package rucafe;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 public class MainMenuController {
+    public void handleClickOrderDonuts(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader;
+            fxmlLoader = new FXMLLoader(getClass().getResource("Donut.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.initStyle(StageStyle.TRANSPARENT);
+            stage.setTitle("Ru Cafe Donut Menu");
+            stage.setScene(new Scene(root1));
+            stage.setX(600);
+            stage.setY(200);
+            stage.showAndWait();
+        } catch (Exception e) {
+            System.out.print("Can't Load New Window");
+        }
+    }
 }
